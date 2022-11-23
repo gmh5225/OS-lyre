db73465be568faa0aa0c0f29f91af0f4f404bc33
mintsuki <mintsuki@protonmail.com>
2022-11-23 03:39:38 +0100

    Revert "kernel: Use standalone limine terminal"
    This reverts commit 820d4d38771584a87abf2eac1e6e130cbf80ec2d.

--- kernel/dev/video/fbdev.c
+++ kernel/dev/video/fbdev.c
@@ -11,7 +11,7 @@
 #include <mm/vmm.h>
 #include <linux/fb.h>
 
-volatile struct limine_framebuffer_request framebuffer_request = {
+static volatile struct limine_framebuffer_request framebuffer_request = {
     .id = LIMINE_FRAMEBUFFER_REQUEST,
     .revision = 0
 };
--- kernel/dev/video/fbdev.h
+++ kernel/dev/video/fbdev.h
@@ -1,8 +1,6 @@
 #ifndef _DEV__VIDEO__FBDEV_H
 #define _DEV__VIDEO__FBDEV_H
 
-extern volatile struct limine_framebuffer_request framebuffer_request;
-
 void fbdev_init(void);
 
 #endif
