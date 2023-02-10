e15be22a32a7eaa72b3c5de7de335caee3731c08
mintsuki <mintsuki@protonmail.com>
2023-02-10 23:06:49 +0100

    kernel: Do not check for deadlocks in sched_yield()

--- kernel/sched/sched.c
+++ kernel/sched/sched.c
@@ -221,7 +221,7 @@ void sched_yield(bool save_ctx) {
     interrupt_toggle(true);
 
     if (save_ctx) {
-        spinlock_acquire(&thread->yield_await);
+        spinlock_acquire_no_dead_check(&thread->yield_await);
         spinlock_release(&thread->yield_await);
     } else {
         for (;;) {
