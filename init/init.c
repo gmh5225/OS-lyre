e08d5951eaa58ddbec8e305d379b4d5e4e754091
Matheus <matheusuky@gmail.com>
2023-02-10 19:26:02 -0300

    init: error checking

--- init/init.c
+++ init/init.c
@@ -11,20 +11,33 @@ int main(void) {
     setenv("PATH", "/usr/local/bin:/usr/bin", 1);
 
     for (;;) {
+
+        if (access("/usr/bin/bash", X_OK) == -1) {
+                perror("init: /usr/bin/bash");
+                return EXIT_FAILURE;
+        }
+
         printf("\nWelcome to Lyre!\n");
         printf("You can find the source code at https://github.com/lyre-os/lyre\n\n");
 
         int pid = fork();
+
+        if (pid == -1) {
+                perror("init: fork failed");
+                return EXIT_FAILURE;
+        }
+
         if (pid == 0) {
             char *argv[] = {"/usr/bin/bash", "-l", NULL};
 
             chdir(getenv("HOME"));
             execvp("/usr/bin/bash", argv);
+            return EXIT_FAILURE;
         } else {
             int status;
             waitpid(pid, &status, 0);
         }
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
